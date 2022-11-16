56f7ffaa88c2e105f362d164047e3f645c2f0c54
antoinettehemlall <hemlall3000@gmail.com>
2022-11-16 10:51:43 -0500

    Create game window

--- collect_coins.py
+++ collect_coins.py
@@ -1,3 +1,30 @@
+import sys
+
 import pygame
 
-pygame.init()
+from settings import Settings
+
+class CollectCoins:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Collect the Coins!")
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen"""
+        self.screen.fill(self.settings.bg_color)
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    cc = CollectCoins()
+    cc.run_game()
+
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """A class to store all settings for game"""
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
